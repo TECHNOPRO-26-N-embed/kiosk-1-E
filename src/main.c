0589aab8d569bd18f7a01b26c1a19d8e95312287
TsubasaMatsunami <282409030+Tsubasa-Matsunami-TP@users.noreply.github.com>
2026-05-18 17:29:54 +0900

    お釣り計算

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void){
+    int choice;
+
+    while(1){
+        printf("\n====================\n");
+        printf(" 自動販売機 メニュー \n");
+        printf("\n====================\n");
+        printf(" 1: 商品選択\n");
+        printf(" 2: 金額投入\n");
+        printf(" 3: 終了\n");
+        printf("----------------------\n");
+        break;
+    }
+    return 0;
+}
